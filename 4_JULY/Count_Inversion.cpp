e30206a7921921138a9c3b94cc0dced2668a0b93
UtkarshDevrani <117424245+UtkarshDevrani@users.noreply.github.com>
2023-07-04 17:07:19 +0530

    Update Count_Inversion.cpp

--- 4_JULY/Count_Inversion.cpp
+++ 4_JULY/Count_Inversion.cpp
@@ -1,1 +1,61 @@
+#define ll long long
+class Solution{
+  
+  public:
 
+ll merge(ll arr[],ll temp[],ll left,ll mid,ll right)
+{
+    ll inv_count=0;
+    ll i = left;
+    ll j = mid;
+    ll k = left;
+    while((i <= mid-1) && (j <= right)){
+        if(arr[i] <= arr[j]){
+            temp[k++] = arr[i++];
+        }
+        else
+        {
+            temp[k++] = arr[j++];
+            inv_count = inv_count + (mid - i);
+        }
+    }
+
+    while(i <= mid - 1)
+        temp[k++] = arr[i++];
+
+    while(j <= right)
+        temp[k++] = arr[j++];
+
+    for(i = left ; i <= right ; i++)
+        arr[i] = temp[i];
+    
+    return inv_count;
+}
+
+ll merge_Sort(ll arr[],ll temp[],ll left,ll right)
+{
+    ll mid,inv_count = 0;
+    if(right > left)
+    {
+        mid = (left + right)/2;
+
+        inv_count += merge_Sort(arr,temp,left,mid);
+        inv_count += merge_Sort(arr,temp,mid+1,right);
+
+        inv_count += merge(arr,temp,left,mid+1,right);
+    }
+    return inv_count;
+}
+
+    // arr[]: Input Array
+    // N : Size of the Array arr[]
+    // Function to count inversions in the array.
+    long long int inversionCount(long long arr[], long long N)
+    {
+        ll temp[N];
+        ll ans = merge_Sort(arr,temp,0,N-1);
+        return ans;
+        // Your Code Here
+    }
+
+};
